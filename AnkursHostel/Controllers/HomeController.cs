4ccbed596273feffefebc8a331f2a17214d5e8a4
Ankur <ashutoshpandey9519@gmail.com>
2023-04-08 20:01:07 +0530

    Intial Commit

---
+++ AnkursHostel/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using AnkursHostel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnkursHostel.Controllers
+{
+    public class HomeController : Controller
+    {
+        AnkursHostelEntities1 db = new AnkursHostelEntities1();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+
+        public ActionResult Features()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(Customer c)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Customers.Add(c);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+
+                    ViewBag.message = "<script>alert('Data Inserted.....')</script>";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.message = "<script>alert('Data Not Inserted !!!!!!')</script>";
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ AnkursHostel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnkursHostel.MvcApplication" Language="C#" %>
